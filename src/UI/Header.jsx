821882ecb5db25fc4a6ff99037032a20bd326333
Mooobi <qkrantod@gmail.com>
2023-05-21 21:25:04 +0900

    Fix Header.js

---
+++ src/UI/Header.jsx
@@ -0,0 +1,39 @@
+import classes from './Header.module.css';
+import logo from '../assets/logo/logo.png';
+import { Link } from 'react-scroll';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className={classes.container}>
+        <section className={classes.side}>
+          <div className={classes.top}>
+            <Link className={classes.nav} to="about" spy={true} smooth={true}>
+              About
+            </Link>
+            <Link className={classes.nav} to="skills" spy={true} smooth={true}>
+              Skills
+            </Link>
+          </div>
+          <div className={classes.bottom} />
+        </section>
+        <Link className={classes.middle} to="main" spy={true} smooth={true}>
+          <img src={logo} alt="logo" className={classes.logo} />
+        </Link>
+        <section className={classes.side}>
+          <div className={classes.top}>
+            <Link className={classes.nav} to="project" spy={true} smooth={true}>
+              Project
+            </Link>
+            <Link className={classes.nav} to="contact" spy={true} smooth={true}>
+              Contact
+            </Link>
+          </div>
+          <div className={classes.bottom} />
+        </section>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
