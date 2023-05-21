81b4c625eb76ae9c8b509d7a5f23902881d6f8c6
Mooobi <qkrantod@gmail.com>
2023-05-21 21:38:45 +0900

    Fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
-import Header from './pages/Header';
+import Footer from './UI/Footer';
+import Header from './UI/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Main from './pages/Main';
+import Project from './pages/Projects';
+import Skills from './pages/Skills';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
     </>
   );
 }
